8fb3704d6b40952e211d25f844b8ab761344c2f2
Narges Masoumi <nmasoumi@asu.edu>
2018-04-19 00:37:08 -0700

    Grid and D

--- src/functions.py
+++ src/functions.py
@@ -16,19 +16,25 @@ plt.plot(x, y,'.')
 plt.show()
 '''
 
-def d_matrix(n, x_i, x_j, c_i, c_j):
+def d_matrix(n, x):
+
+    def C(idx):
+        if idx == 0 or idx == n:
+            return 2
+        else:
+            return 1
 
     d = np.zeros((n+1, n+1))
     for i in range (0, n+1):
         for j in range(0, n+1):
             if i==j and 1 <= j <= n-1:
-                d[i,j] = -x_i / 2* (1-x_j**2)
+                d[i,j] = -x[i] / 2* (1-x[j]**2)
             elif i==j==0:
                 d[i,j] = 2* n**2 + 1 / 6
             elif i==j== n:
                 d[i, j] = -2 * n ** 2 + 1 / 6
             else:
-                d[i,j] = c_i*(-1)**(i+j) / c_j *(x_i - x_j)
+                d[i,j] = C(i) * (-1)**(i+j) / C(j) * (x[i] - x[j])
 
     return d
 
